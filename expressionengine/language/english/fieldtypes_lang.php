ea4ff6e0db4a1b71efdd9c73fe3c20239e06e6a5
Wes Baker <wes@wesbaker.com>
2012-06-07 09:44:28 -0400

    Committing language files for 2.5.2.

---
+++ expressionengine/language/english/fieldtypes_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang = array(
+
+'add_file'	=>
+'Add File',
+
+'remove_file' =>
+'Remove File',
+
+'directory_no_access' =>
+'You do not have access to the directory specified for this field',
+
+'directory' =>
+'Directory:',
+
+// IGNORE
+''=>'');
+/* End of file fieldtypes_lang.php */
+/* Location: ./system/expressionengine/language/english/fieldtypes_lang.php */
