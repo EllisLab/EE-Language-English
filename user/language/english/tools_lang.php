8e2cb3441838bf3b65087c497a15fb69b90d4b4e
Emmanuel Beauchamps <e.bops@hotmail.fr>
2015-10-23 18:04:28 +0200

    EE3

--- user/language/english/tools_lang.php
+++ user/language/english/tools_lang.php
@@ -64,43 +64,9 @@ $lang = array(
 
 
 /* Translation */
-<<<<<<< HEAD:expressionengine/language/english/tools_lang.php
-'translation_dir_unwritable' =>
-'Warning: Your translation directory is not writable.',
-
-/* Developer Log */
-'log_id'				=> 'Log ID',
-'log_message'			=> 'Message',
-'deprecated_function'	=> 'Deprecated function %s called',
-'deprecated_on_line'	=> 'in %s on line %d.',
-'deprecated_since'		=> 'Deprecated since %s.',
-'deprecated_use_instead'	=> 'Use %s instead.',
-'deprecated_template'	=> 'From template tag %s in %s.',
-'deprecated_snippets'	=> 'This tag may have been parsed from one of these snippets: %s',
-'deprecation_detected'	=> 'This is a log for developers to bring notices or warnings to the attention of the Super Admins.',
-'dev_log_view_report'	=> 'View report',
-'dev_log_help'			=> 'What does this mean?',
-'deprecated_meaning'	=>
-'<p>One of two things has happened:</p>
-<ol>
-	<li>The system has detected native code or an add-on that is using outdated code that may stop working or cause issues with the system. This means an add-on in your installation is accessing a part of ExpressionEngine\'s core code which EllisLab plans to remove in the future in order to improve the quality of the product.</li>
-	<li>A warning or message was raised by native code or an add-on for the Super Admin to view.</li>
-</ol>
-<p>To remedy this notice, you may need to do any of the following:</p>
-<ul>
-	<li>Contact the developer who manages your ExpressionEngine installation for help</li>
-	<li>Make sure you have the latest version of all your installed add-ons</li>
-	<li>Contact the developer of the offending add-on to make sure they\'re aware of the problem and plans to issue an update</li>
-</ul>',
-
-
-'screen_name' =>
-'Screen Name',
-=======
 'translation_dir_unwritable' => 'Warning: Your translation directory is not writable.',
 
 'screen_name' => 'Screen Name',
->>>>>>> origin/master:user/language/english/tools_lang.php
 
 
 ''=>''
